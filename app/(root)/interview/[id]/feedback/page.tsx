92d89b9feb03206ff3dd56eb9117784a031fa853
Salah Eddine Boussettah <boussettahsallah@gmail.com>
2025-03-26 03:17:13 +0000

    Fixing Retake button rediraction in feedback page

--- app/(root)/interview/[id]/feedback/page.tsx
+++ app/(root)/interview/[id]/feedback/page.tsx
@@ -89,7 +89,7 @@ const Page = async ({ params }: RouteParams) => {
                 </Button>
 
                 <Button className='btn-primary flex-1'>
-                    <Link href="/interview/${id}" className='flex w-full justify-center'>
+                    <Link href={`/interview/${id}`} className='flex w-full justify-center'>
                         <p className='text-sm font-semibold text-black text-center'>
                             Retake Interview
                         </p>
